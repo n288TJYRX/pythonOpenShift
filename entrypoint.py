b80d9b884925cc3b44a826f1fea4d7a565231b77
n288TJYRX <n288TJYRX@tutanota.com>
2019-10-07 12:49:06 +0100

    Getting simple response from python server

--- entrypoint.py
+++ entrypoint.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+import time
 from DataInputStream import DataInputStream
 
 HOST = socket.gethostbyname(socket.gethostname())
@@ -15,25 +16,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print('Waiting for incoming connections...')
     # Wait for incoming connections
     conn, addr = s.accept()  
-    conn.sendall(struct.pack('?', True))
+    conn.sendall('Connected to the python server!'.encode('utf-8'))
     print('Connected to: ', addr)
     # Keep listening for requests to run scripts
-    while True:
-        dis = DataInputStream(conn)
-        if dis:
-            # Check the data is in the right format
-            currentPayload = dis.read_utf()
-            while currentPayload != bytes(']', encoding='utf-8'):
-                if rawData is None:
-                    rawData = bytes() + currentPayload
-                else:
-                    currentPayload = dis.read_utf()
-                    rawData += currentPayload
-            data = rawData.decode('utf-8')
+        # if dis:
+            # # Check the data is in the right format
+            # data = dis.read_utf()
 
-            # Run the right script passing in the parameters
-            # (Do nothing with the data at the moment)
+            # # Run the right script passing in the parameters
+            # # (Do nothing with the data at the moment)
 
-            # Send the results back to the server
-            # (At the moment just send the data back)
-            conn.sendall(data.encode('utf-8'))
+            # # Send the results back to the server
+            # # (At the moment just send the data back)
+            # if data:
+            #     conn.sendall(data.encode('utf-8'))
